c7099da8affc49154c6d1b24758a03a8b515162e
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-07-10 15:20:41 +0530

    [Mukesh] add:swap and tables

---
+++ Demo-session/Swap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_session
+{
+    internal class Swap
+    {
+        public static void num()
+        {
+            Console.WriteLine("Enter a value");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter b value");
+            int b = Convert.ToInt32(Console.ReadLine()); 
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine(a + " " + b);
+
+
+
+
+        }
+    }
+}
---
+++ Demo-session/Tables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_session
+{
+    class Tables
+    {
+        public static void table()
+        {
+            Console.WriteLine("Enter a number");
+            int input = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(input * i);
+            }
+        }
+    }
+}
